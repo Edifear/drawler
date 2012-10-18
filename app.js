b4865e96538ef3afeb7856c18013b838a5790b0a
Edifer <edifer@edifer-TravelMate-5742G.(none)>
2012-10-18 14:47:34 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(process.env.VMC_APP_PORT || 8000);
+
+app.use(app.router);
+app.use(express.static(__dirname + "/static"))
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/drawing.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('line', function (data) {
+      socket.broadcast.emit('line', data);
+    });
+
+    socket.on('msg', function (data) {
+      socket.broadcast.emit('msg', data);
+    });
+
+});
