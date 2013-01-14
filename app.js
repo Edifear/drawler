0febdb630629037f3c9dadf190286674753f9332
edifear <edifear@gmail.com>
2013-01-14 19:29:37 +0200

    set problems ))

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 var express = require('express'),
     app = express(),
     http = require('http'),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server);
+    server = http.createServer(app);
 
-server.listen(process.env.VMC_APP_PORT || 80);
+server.listen(process.env.VMC_APP_PORT || 8000);
 
 app.use(app.router);
 app.use(express.static(__dirname + "/static"))
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/drawing.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
+app.get('/drawling', function (req, res) {
+    res.sendfile(__dirname + '/drawing.html');
 
-    socket.on('line', function (data) {
-      socket.broadcast.emit('line', data);
+    var io = require('socket.io').listen(server, {'log level': 2});
+
+    io.sockets.on('connection', function (socket) {
+
+        socket.on('line', function (data) {
+          socket.broadcast.emit('line', data);
+        });
+
+        socket.on('msg', function (data) {
+          socket.broadcast.emit('msg', data);
+        });
+
     });
+});
 
-    socket.on('msg', function (data) {
-      socket.broadcast.emit('msg', data);
-    });
-
+app.get('/problems/:id', function (req, res) {
+    res.sendfile(__dirname + '/problems/problem-'+req.params.id+'.html');
 });
