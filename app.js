073fc863d0b13ffed5b9a27adb3c28a0a2a46e68
Edifear <edifear@gmail.com>
2013-01-15 09:56:05 +0200

    curve drawling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     http = require('http'),
-    server = http.createServer(app);
+    server = http.createServer(app),
+    io = require('socket.io').listen(server, {'log level': 1});
 
 server.listen(process.env.VMC_APP_PORT || 8000);
 
@@ -11,25 +12,23 @@ app.use(express.static(__dirname + "/static"))
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
-
 app.get('/drawling', function (req, res) {
     res.sendfile(__dirname + '/drawing.html');
-
-    var io = require('socket.io').listen(server, {'log level': 2});
-
-    io.sockets.on('connection', function (socket) {
-
-        socket.on('line', function (data) {
-          socket.broadcast.emit('line', data);
-        });
-
-        socket.on('msg', function (data) {
-          socket.broadcast.emit('msg', data);
-        });
-
-    });
 });
-
 app.get('/problems/:id', function (req, res) {
     res.sendfile(__dirname + '/problems/problem-'+req.params.id+'.html');
 });
+
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('line', function (data) {
+        socket.broadcast.emit('line', data);
+    });
+
+    socket.on('msg', function (data) {
+        socket.broadcast.emit('msg', data);
+    });
+
+});
+
