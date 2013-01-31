682bb1eec20ef95d4ee069d849c915481b4e7ac5
Edifear <edifear@gmail.com>
2013-01-31 11:43:58 +0200

    granade

--- static/js/ball.js
+++ static/js/ball.js
@@ -10,7 +10,7 @@ function Ball (radius, color) {
   this.scaleX = 1;
   this.scaleY = 1;
   this.color = utils.parseColor(color);
-  this.lineWidth = 1;
+  this.lineWidth = 0;
 }
 
 Ball.prototype.draw = function (context) {
@@ -18,7 +18,7 @@ Ball.prototype.draw = function (context) {
   context.translate(this.x, this.y);
   context.rotate(this.rotation);
   context.scale(this.scaleX, this.scaleY);
-  
+
   context.lineWidth = this.lineWidth;
   context.fillStyle = this.color;
   context.beginPath();
